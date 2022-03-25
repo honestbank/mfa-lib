5c0941ffe759dc683c49c6abbc44fa155343574c
James <james@honestbank.com>
2022-03-25 15:00:54 +0700

    feat: add read identifier and jwt (#11)

--- flow/entities/flow.go
+++ flow/entities/flow.go
@@ -18,6 +18,9 @@ func (f Flow) GetName() string {
 	return f.Name
 }
 
+type identifier struct{}
+type jwt struct{}
+
 func (f Flow) Solve(ctx context.Context, challenge string, input string, JWTData mfaEntities.JWTData) (*map[string]interface{}, error) {
 	var marshaledInput map[string]interface{}
 	err := json.Unmarshal([]byte(input), &marshaledInput)
@@ -42,6 +45,34 @@ func (f Flow) Request(ctx context.Context, challenge string, input string, JWTDa
 	}
 
 	return nil, errors.New("Challenge not found")
+}
+
+func (f Flow) SetIdentifier(ctx context.Context, value string) context.Context {
+	return context.WithValue(ctx, &identifier{}, value)
+}
+
+func (f Flow) GetIdentifier(ctx context.Context) *string {
+	if value := ctx.Value(&identifier{}); value != nil {
+		valString := value.(string)
+
+		return &valString
+	}
+
+	return nil
+}
+
+func (f Flow) SetJWT(ctx context.Context, value string) context.Context {
+	return context.WithValue(ctx, &jwt{}, value)
+}
+
+func (f Flow) GetJWT(ctx context.Context) *string {
+	if value := ctx.Value(&jwt{}); value != nil {
+		valString := value.(string)
+
+		return &valString
+	}
+
+	return nil
 }
 
 func (f Flow) GetChallenges() []string {

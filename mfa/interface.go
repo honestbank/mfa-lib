c0d53bdb8015f48ef91e300f5ea69dfd8cc3c244
James Trotter <james@honestbank.com>
2022-03-25 15:58:38 +0700

    fix: interface not updated

--- mfa/interface.go
+++ mfa/interface.go
@@ -8,5 +8,5 @@ import (
 
 type IMFAService interface {
 	Request(ctx context.Context, flow string) (*entities.MFAResult, error)
-	Process(ctx context.Context, jwt string, challenge string, input string, request bool, beforeHook *func(ctx context.Context, challenge string, input string) error) (*entities.MFAResult, error)
+	Process(ctx context.Context, jwt string, challenge string, input string, request bool, beforeHook *func(ctx context.Context, challenge string, input string) (context.Context, error)) (*entities.MFAResult, error)
 }

31e59ffb42ca28792e14e151b1d46cc542be72f8
James <james@honestbank.com>
2022-09-05 11:21:42 +0700

    fix: should set challenge status on request (#28)

--- mfa/mfa_test.go
+++ mfa/mfa_test.go
@@ -264,6 +264,7 @@ func TestNewMFAService(t *testing.T) {
 		a.Equal("initialize error", err.Error())
 		a.Nil(result)
 	})
+
 	t.Run("MFA_Request_PassAdditional", func(t *testing.T) {
 		validJWT := "nil.eyJmbG93IjoidGVzdCIsImNoYWxsZW5nZXMiOnsiZHVtbXkiOnsic3RhdHVzIjoicGVuZGluZyJ9fSwiaWRlbnRpZmllciI6ImlkZW50aWZpZXIiLCJ0eXBlIjoic29tZXR5cGUiLCJtZXRhIjpbeyJrZXkiOiJtZXRha2V5IiwidmFsdWUiOiJtdWNoTWV0YSJ9XX0."
 		a := assert.New(t)
@@ -330,6 +331,45 @@ func TestNewMFAService(t *testing.T) {
 		a.Equal("metakey", decodedJWT.Meta[0].Key)
 
 		a.NoError(err)
+		a.Equal(expectedResult, *result)
+	})
+
+	t.Run("MFA_Process_request_error", func(t *testing.T) {
+		validJWT := "nil.eyJmbG93IjoidGVzdCIsImNoYWxsZW5nZXMiOnsiZHVtbXkiOnsic3RhdHVzIjoicGVuZGluZyJ9fX0."
+		a := assert.New(t)
+		ctrl := gomock.NewController(t)
+		config := entities.Config{}
+		jwtService := mocks.NewMockIJWTService(ctrl)
+		mockflow := mocks.NewMockIFlow(ctrl)
+
+		flowMap := map[string]flow.IFlow{
+			"test": mockflow,
+		}
+
+		jwtService.EXPECT().GenerateToken(gomock.Any(), gomock.Any()).Return(validJWT, nil)
+
+		mockflow.EXPECT().Request(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, &entities.MFAError{
+			Code:     "ERROR",
+			Message:  "error",
+			Metadata: nil,
+		})
+		mockflow.EXPECT().Validate(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, nil)
+		mockflow.EXPECT().GetName().Return("test")
+		mockflow.EXPECT().GetChallenges(gomock.Any(), gomock.Any(), gomock.Any()).Return([]string{"dummy"})
+		mockflow.EXPECT().GetChallenges(gomock.Any(), gomock.Any(), gomock.Any()).Return([]string{"dummy"})
+
+		mfaService := mfa.NewMFAService(config, jwtService, flowMap)
+
+		result, err := mfaService.Process(context.TODO(), validJWT, "dummy", "{}", true, nil)
+
+		expectedResult := entities.MFAResult{
+			Token:      validJWT,
+			Challenges: []string{"dummy"},
+			Metadata:   nil,
+		}
+
+		a.Error(err)
+		a.Equal("error", err.Error())
 		a.Equal(expectedResult, *result)
 	})
 
